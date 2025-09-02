eaf4a82c6d68455b86f987a3d2cbb2fe94be5544
Luis-Gabriel Mendez Barreiro <luisgabrielmendez@hotmail.com>
2025-09-01 22:26:53 -0600

    [Sprint2][Task 0] chg: Remove all warnings

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,5 +1,4 @@
 use auth_service::Application;
-use axum::{response::Html};
 
 #[tokio::main]
 async fn main() {
@@ -10,7 +9,3 @@ async fn main() {
     app.run().await.expect("Failed to run app");
 }
 
-async fn hello_handler() -> Html<&'static str> {
-    // DONE: Update this to a custom message!
-    Html("<h1>Hello, I'm excited to start my new professional life!</h1>")
-}
--- auth-service/src/routes/signup.rs
+++ auth-service/src/routes/signup.rs
@@ -1,7 +1,7 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::Deserialize;
 
-pub async fn signup(Json(request): Json<SignupRequest>) -> impl IntoResponse {
+pub async fn signup(Json(_request): Json<SignupRequest>) -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
 
