4ff8fab0bd1f5e6b5e004127cc847c8b7e89b478
Luis-Gabriel Mendez Barreiro <luisgabrielmendez@hotmail.com>
2025-09-10 16:35:34 -0600

    [Sprint 3] [Task 1] new: Implement log-in route success case

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,6 +1,47 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
 
-pub async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, Email, Password}
+};
+
+pub async fn login(
+    State(state): State<AppState>,
+    Json(request): Json<LoginRequest>
+) -> Result<impl IntoResponse, AuthAPIError> {
+    //                                //  Validate request content
+    let password = match Password::parse(request.password) {
+        Ok(password) => password,
+        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+    };
+
+    let email = match Email::parse(request.email) {
+        Ok(email) => email,
+        Err(_) => return Err(AuthAPIError::InvalidCredentials)
+    };
+
+    let user_store = &state.user_store.read().await;
+
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return Err(AuthAPIError::IncorrectCredentials)
+    }
+
+    let _user = match user_store.get_user(&email).await {
+        Ok(user) => user,
+        Err(_) => return Err(AuthAPIError::IncorrectCredentials),
+    };
+
+    Ok(StatusCode::OK.into_response())
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct LoginResponse {
+    pub message: String,
+}
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,11 +1,179 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn authenticate_user_and_return_jwt() {
+async fn should_return_422_if_malformed_credentials() {
+    //                                //  DONE-TODO:
+    //                                //  Add 422 test case
+    //                                //  To test malformed credentials with login:
+    //                                //  1. Create user to login.
+    //                                //  2. Create data for test cases.
+    //                                //  3. Run test cases
+
+    //                                //  PREPARE TEST ENVIRONMENT
+    //                                //  Create app instance
     let app = TestApp::new().await;
 
-    let response = app.post_login().await;
+    //                                //  1. Create user to login
+    //                                //  Create data for test case
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
-    // assert_eq!(response.headers().get("content-type").unwrap(), "text/html");
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": false
+    });
+
+    //                                //  Run test case
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    //                                //  2. Create data for test cases.
+    let test_cases = [
+        //                            //  Missing email
+        serde_json::json!({
+            "password": "password123",
+        }),
+        //                            //  Missing password
+        serde_json::json!({
+            "email": random_email,
+        }),
+        //                            //  Missing email and password
+        serde_json::json!({})
+    ];
+
+    //                                //  3. Run test cases
+    for test_case in test_cases {
+        let response = app.post_login(&test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+
+}
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    //                                //  DONE-TODO:
+    //                                //  Call the log-in route with invalid credentials and assert that a
+    //                                //  400 HTTP status code is returned along with the appropriate error
+    //                                //  message.
+    
+    //                                //  PREPARE TEST ENVIRONMENT
+    //                                //  Create app instance
+    let app = TestApp::new().await;
+
+    //                                //  1. Create user to login
+    //                                //  Create data for test case
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": false
+    });
+
+    //                                //  Run test case
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+    //                                //  2. Create data for test cases.
+    let test_cases = [
+        //                            //  Invalid email
+        serde_json::json!({
+            "email": "invalid_email",
+            "password": "password123",
+        }),
+        //                            //  Invalid password
+        serde_json::json!({
+            "email": random_email,
+            "password": "invalid",
+        }),
+        //                            //  Missing email
+        serde_json::json!({
+            "email": "",
+            "password": "password123",
+        }),
+        //                            //  Missing password
+        serde_json::json!({
+            "email": random_email,
+            "password": "",
+        }),
+        //                            //  Missing email and password
+        serde_json::json!({
+            "email": "",
+            "password": "",
+        })
+    ];
+
+    for test_case in test_cases {
+        let response = app.post_login(&test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    //                                //  TODO:
+    //                                //  Call the log-in route with incorrect credentials and assert that a
+    //                                //  401 HTTP status code is returned along with the appropriate error
+    //                                //  message.
+    
+    //                                //  PREPARE TEST ENVIRONMENT
+    //                                //  Create app instance
+    let app = TestApp::new().await;
+
+    //                                //  1. Create user to login
+    //                                //  Create data for test case
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": false
+    });
+
+    //                                //  Run test case
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+    //                                //  2. Create data for test cases.
+    let test_cases = [
+        //                            //  Wrong password
+        serde_json::json!({
+            "email": random_email,
+            "password": "wrong-password",
+        }),
+        //                            //  Wrong email
+        serde_json::json!({
+            "email": "wrong@email.com",
+            "password": "password123",
+        }),
+        //                            //  Wrong email and password
+        serde_json::json!({
+            "email": "wrong@email.com",
+            "password": "wrong-password",
+        }),
+    ];
+
+    for test_case in test_cases {
+        let response = app.post_login(&test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            401,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
